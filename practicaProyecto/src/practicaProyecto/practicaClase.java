c562f17ab28f5623673050bde55e7d87c44e8980
rafa <rafa@rafa>
2018-05-01 18:17:48 +0200

    V5

--- practicaProyecto/src/practicaProyecto/practicaClase.java
+++ practicaProyecto/src/practicaProyecto/practicaClase.java
@@ -5,7 +5,7 @@ public class practicaClase {
 	public static void main(String[] args) {
 		System.out.println("Esta es la practica de Sonia");
 		System.out.println("Hola");
-		//comentario
+		//comentario  
 
 	}
 
