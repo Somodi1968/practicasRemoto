708b75bf5bdac16ed4b37eb66f1f59471416a060
Usuario <Usuario@Usuario-PC>
2018-05-01 18:15:00 +0200

    respuesta1

--- practicaProyecto/src/practicaProyecto/practicaClase.java
+++ practicaProyecto/src/practicaProyecto/practicaClase.java
@@ -4,7 +4,7 @@ public class practicaClase {
 
 	public static void main(String[] args) {
 		System.out.println("Esta es la practica de Sonia");
-		System.out.println("Hola");
+		System.out.println("Hola, adiós");
 
 	}
 
